40f1b9130ccb2a8fa9f0aca856ab9e3ecd12abb6
github username <github email address>
2018-10-24 13:28:48 -0400

    testing123

---
+++ src/presentational/Chart.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+
+
+
+
+class Chart extends Component {
+
+  render() {
+
+    return (
+      <div>
+        OOOOGA BOOOOOGA TESTING THIS BRANCH!!!
+    </div>
+    );
+  }
+
+}
+
+
+
+export default Chart;
